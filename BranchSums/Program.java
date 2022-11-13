d731acdf00e21ad77499624dee9dc93890e2aa72
Vinay Babu Yella <60830655+vinaygitty@users.noreply.github.com>
2022-11-13 00:44:14 -0800

    Create Program.java

---
+++ BranchSums/Program.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+
+class Program {
+  public static class BinaryTree {
+    int value;
+    BinaryTree left;
+    BinaryTree right;
+
+    BinaryTree(int value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+  public static List<Integer> branchSums(BinaryTree root) {
+   List<Integer> sums = new ArrayList<Integer>();
+    calculateBranchSums(root,0,sums);
+    return sums;
+  }
+
+  public static void calculateBranchSums(BinaryTree node, int runningSum, List<Integer> sums)
+  {
+    if (node == null) return;
+
+    int newRunningSum = runningSum + node.value;
+    if (node.left == null && node.right ==null)
+     {
+     sums.add(newRunningSum);
+     return;
+     }
+
+     calculateBranchSums(node.left,newRunningSum,sums );
+     calculateBranchSums(node.right,newRunningSum,sums );
+
+  }
+  
