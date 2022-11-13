f881b5c2fb1552485f12df30d65a46d486c4a87e
Vinay Babu Yella <60830655+vinaygitty@users.noreply.github.com>
2022-11-13 14:03:48 -0800

    Create Program.java

---
+++ Remove Dups from LinkedList/Program.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Program {
+ 
+  public static class LinkedList {
+    public int value;
+    public LinkedList next;
+
+    public LinkedList(int value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  // O(n) time | O(1) space
+  public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+
+    LinkedList currentNode = linkedList;
+    
+    while(currentNode != null){
+      LinkedList nextDistinctNode = currentNode.next;
+      while (nextDistinctNode!= null && nextDistinctNode.value == currentNode.value){
+        nextDistinctNode = nextDistinctNode.next;
+      }
+        currentNode.next = nextDistinctNode;
+        currentNode = nextDistinctNode;
+
+      
+    }
+    return linkedList;
+
+  }
+
+
+
+  
+}
